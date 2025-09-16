7b3f5afaad7648d6b9d96105564b88453da8d1e7
Lee Seongwon <qkakzm@gmail.com>
2025-09-16 20:45:19 +0900

    2557,1000,1001

---
+++ src/b250916/b1000.java
@@ -0,0 +1,16 @@
+package b250916;
+
+import java.util.Scanner;
+
+public class b1000 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a + b);
+	}
+
+}
